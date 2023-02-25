982c7acae86afe87a7998cb64e32940bdc1bc236
HJeong1200 <hjeong1200@gmail.com>
2023-02-25 21:16:50 +0900

    feat - #2 add treeview

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,42 @@
 import { Collapse } from "antd";
+import { TreeView, TreeItem } from "@mui/lab";
+import range from "../functions/range";
 
 const Sidebar = () => {
   const { Panel } = Collapse;
+  const files = range(1, 10);
 
   return (
     <aside>
       <Collapse>
-        <Panel header="Category 1" key="category_1"></Panel>
-        <Panel header="Category 2" key="category_2"></Panel>
+        <Panel header="Category 1" key="category_1">
+          <TreeView>
+            <TreeItem nodeId="Folder 1" label="Folder 1"></TreeItem>
+            <TreeItem nodeId="Folder 2" label="Folder 2">
+              {files.map((num) => (
+                <TreeItem
+                  key={num}
+                  nodeId={`File ${num}`}
+                  label={`File ${num}`}
+                ></TreeItem>
+              ))}
+            </TreeItem>
+          </TreeView>
+        </Panel>
+        <Panel header="Category 2" key="category_2">
+          <TreeView>
+            <TreeItem nodeId="Folder 1" label="Folder 1"></TreeItem>
+            <TreeItem nodeId="Folder 2" label="Folder 2">
+              {files.map((num) => (
+                <TreeItem
+                  key={num}
+                  nodeId={`File ${num}`}
+                  label={`File ${num}`}
+                ></TreeItem>
+              ))}
+            </TreeItem>
+          </TreeView>
+        </Panel>
       </Collapse>
     </aside>
   );
