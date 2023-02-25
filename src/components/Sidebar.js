78aaf4cd1d152dca942b002944de47b8c13947cb
HJeong1200 <hjeong1200@gmail.com>
2023-02-25 21:28:36 +0900

    feat - #3 add re-resizable and scrollbar settings

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { Collapse } from "antd";
+import { Resizable } from "re-resizable";
 import Category from "./sidebar/Category";
 
 const Sidebar = () => {
@@ -8,7 +9,18 @@ const Sidebar = () => {
     <aside>
       <Collapse>
         <Panel header="Category 1" key="category_1">
-          <Category />
+          <Resizable
+            defaultSize={{
+              width: "100%",
+              height: "277",
+            }}
+            enable={{
+              bottom: true,
+            }}
+            style={{ overflowX: "hidden", overflowY: "auto" }}
+          >
+            <Category />
+          </Resizable>
         </Panel>
         <Panel header="Category 2" key="category_2">
           <Category />
